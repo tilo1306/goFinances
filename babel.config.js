636d2240da7823f4058c9f592a831038d32ce148
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-11 20:54:01 -0300

    chore: add global/styles alias

--- babel.config.js
+++ babel.config.js
@@ -13,7 +13,7 @@ module.exports = function (api) {
             "@routes": "./src/routes",
             "@screens": "./src/screens",
             "@storage": "./src/storage",
-            "@theme": "./src/theme",
+            "@global": "./src/global",
             "@utils": "./src/utils",
           },
         },
