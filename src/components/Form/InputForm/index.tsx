57d6b65d295a1cbaccdbdc8e751abf3cedb3672d
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-14 19:51:18 -0300

    feat: add inputForm

--- src/components/Form/InputForm/index.tsx
+++ src/components/Form/InputForm/index.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import { TextInputProps } from "react-native";
-import { Container } from "./styles";
+import { Container, Error } from "./styles";
 import { Input } from "../Input";
 import { Control, Controller } from "react-hook-form";
 
 type Props = TextInputProps & {
   control: Control;
   name: string;
+  error?: string;
 };
 
-export function InputForm({ control, name, ...rest }: Props) {
+export function InputForm({ control, name, error, ...rest }: Props) {
   return (
     <Container>
       <Controller
         control={control}
-        render={({ field: { onchange, value } }) => (
-          <Input onChangeText={onchange} value={value} {...rest} />
+        render={({ field: { onChange, value } }) => (
+          <Input onChangeText={onChange} value={value} {...rest} />
         )}
         name={name}
       />
+      {error && <Error>{error}</Error>}
     </Container>
   );
 }
