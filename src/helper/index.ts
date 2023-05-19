e603cb8e17d1ed9fe93d2f0368af9bfcacbc6020
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-19 12:54:38 -0300

    feat: create helper calculate

---
+++ src/helper/index.ts
@@ -0,0 +1,114 @@
+import { ITransaction } from "../type";
+
+export function calculate(data: ITransaction[]) {
+  if (data.length === 0) {
+    return {
+      entries: {
+        amount: Number(0).toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }),
+      },
+      expensives: {
+        amount: Number(0).toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }),
+      },
+      total: {
+        amount: Number(0).toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }),
+      },
+    };
+  } else {
+    const entriesTotal = data
+      .filter((transaction) => transaction.type === "positive")
+      .reduce(
+        (acc, value) =>
+          Number(acc) +
+          parseFloat(
+            value.amount
+              .replace("R$", "")
+              .trim()
+              .replace(".", "")
+              .replace(",", ".")
+          ),
+        0
+      );
+
+    const expensiveTotal = data
+      .filter((transaction) => transaction.type === "negative")
+      .reduce(
+        (acc, value) =>
+          Number(acc) +
+          parseFloat(
+            value.amount
+              .replace("R$", "")
+              .trim()
+              .replace(".", "")
+              .replace(",", ".")
+          ),
+        0
+      );
+    const total = entriesTotal - expensiveTotal;
+
+    const lastTransactionEntries = getLastTransactionDate(data, "positive");
+    const lastTransactionExpensives = getLastTransactionDate(data, "negative");
+    const totalInterval = `01 A ${lastTransactionExpensives}`;
+
+    return {
+      entries: {
+        amount: entriesTotal.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }),
+        lastTransaction: lastTransactionEntries,
+      },
+      expensives: {
+        amount: expensiveTotal.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }),
+        lastTransaction: lastTransactionExpensives,
+      },
+      total: {
+        amount: total.toLocaleString("pt-br", {
+          style: "currency",
+          currency: "BRL",
+        }),
+        lastTransaction: totalInterval,
+      },
+    };
+  }
+}
+
+export function getLastTransactionDate(
+  collection: ITransaction[],
+  type: "positive" | "negative"
+) {
+  const filteredTransactions = collection.filter(
+    (transaction) => transaction.type === type
+  );
+
+  const filterLastTransaction = filteredTransactions.reduce(
+    (maxDate, transaction) => {
+      if (transaction.date) {
+        const [day, month, year] = transaction.date.split("/");
+        const timestamp = new Date(`20${year}-${month}-${day}`).getTime();
+        return isNaN(timestamp) ? maxDate : Math.max(maxDate, timestamp);
+      }
+      return maxDate;
+    },
+    0 as number
+  );
+
+  const dateTransaction =
+    filterLastTransaction === 0 ? undefined : new Date(filterLastTransaction);
+
+  return `${dateTransaction?.getDate()} de ${dateTransaction?.toLocaleString(
+    "pt-BR",
+    { month: "long" }
+  )}`;
+}
