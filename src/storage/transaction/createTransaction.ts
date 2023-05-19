8e2448e3e09424c00d18157941b55f86d7f60294
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-19 12:57:14 -0300

    feat: createTransaction storage

--- src/storage/transaction/createTransaction.ts
+++ src/storage/transaction/createTransaction.ts
@@ -5,11 +5,30 @@ import { transactionsGetAll } from "./transactionsGetAll";
 import { TRANSACTION_COLLECTION } from "@storage/storageConfig";
 import { ITransaction } from "src/type";
 
-export async function createTransaction(date: ITransaction) {
+export async function createTransaction(data: ITransaction) {
   try {
     const storeTransactions = await transactionsGetAll();
 
-    const storage = JSON.stringify([...storeTransactions, date]);
+    const amount = Number(data.amount).toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+    const date = Intl.DateTimeFormat("pt-br", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "2-digit",
+    }).format(new Date());
+
+    const newTransaction: ITransaction = {
+      id: data.id,
+      title: data.title,
+      amount,
+      type: data.type,
+      category: data.category,
+      date,
+    };
+
+    const storage = JSON.stringify([...storeTransactions, newTransaction]);
     await AsyncStorage.setItem(TRANSACTION_COLLECTION, storage);
   } catch (error) {
     throw error;
