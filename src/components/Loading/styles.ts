c2cf613e116b4c49662f953b1576bbf41ea81cfd
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-19 16:48:41 -0300

    refact: color container primary from shade

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.shape};
   flex: 1;
   justify-content: center;
 `;
