e1556d5b4885051fae6606853c2af15a456bfe60
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-20 18:22:13 -0300

    feat: add USER_COLLECTION

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
 const TRANSACTION_COLLECTION = "@gofinances:transactions";
+const USER_COLLECTION = "@gofinances:user";
 
-export { TRANSACTION_COLLECTION };
+export { TRANSACTION_COLLECTION, USER_COLLECTION };
