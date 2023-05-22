15bfb1219c9c6b88a815e6ab04eb101e47257e6d
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-05-22 19:06:25 -0300

    feat: filter transaction user

--- src/storage/transaction/transactionsGetAll.ts
+++ src/storage/transaction/transactionsGetAll.ts
@@ -2,13 +2,19 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { TRANSACTION_COLLECTION } from "@storage/storageConfig";
 import { ITransaction } from "src/type";
 
-export async function transactionsGetAll() {
+export async function transactionsGetAll(userId: string) {
   try {
     const storage = await AsyncStorage.getItem(TRANSACTION_COLLECTION);
 
-    const transactions: ITransaction[] = storage ? JSON.parse(storage) : [];
+    if (storage) {
+      const listStorage: ITransaction[] = JSON.parse(storage);
+      const findTransactionsUser = listStorage.filter(
+        (transaction) => transaction.userId === userId
+      );
+      return findTransactionsUser;
+    }
 
-    return transactions;
+    return [];
   } catch (error) {
     throw error;
   }
